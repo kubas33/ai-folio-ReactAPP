3ae7de9fb00692846fd1d254a5fce36aa081d882
pow4h <44829224+kubas33@users.noreply.github.com>
2023-10-04 11:31:33 +0000

    Fix index of random photo and improve semantic and styles

--- src/components/ImageCard.tsx
+++ src/components/ImageCard.tsx
@@ -1,37 +1,57 @@
-import {PropsWithChildren} from 'react';
-import { Col, ButtonGroup, Button } from 'react-bootstrap';
-import {FiMoreVertical} from 'react-icons/fi'
+import { PropsWithChildren } from "react";
+import { Col, ButtonGroup, Button, Image } from "react-bootstrap";
+import { FiMoreVertical } from "react-icons/fi";
 import { AiOutlineEdit, AiOutlineEye, AiOutlineDelete } from "react-icons/ai";
 
 interface ImageCardProps extends PropsWithChildren {
-    fileName: string
+  fileName: string;
 }
 
-export default function  ImageCard(props: ImageCardProps) {
-    const index : number = Math.random() * 100;
-    return (
-        <Col>
-            <div className='image-card m-1'>
-                <img src={`https://picsum.photos/800/600?random=${index}`} alt="" />
-                <div className='context-menu-top'>
-                    <Button variant="outline-light" size="sm" onClick={() => console.log("Context")}>
-                        <FiMoreVertical/>
-                    </Button>
-                </div>
-                <div className="context-menu-bottom">
-                    <ButtonGroup>
-                        <Button variant="outline-light" size="sm" onClick={() => console.log("Edit")}>
-                            <AiOutlineEdit/>
-                        </Button>
-                        <Button variant="outline-light" size="sm" onClick={() => console.log("View")}>
-                            <AiOutlineEye/>
-                        </Button>
-                        <Button variant="outline-light" size="sm" onClick={() => console.log("Delete")}>
-                            <AiOutlineDelete/>
-                        </Button>
-                    </ButtonGroup>
-                </div>
-            </div>
-        </Col>
-    )
+export default function ImageCard(props: ImageCardProps) {
+  const index: number = Math.floor(Math.random() * 100);
+  return (
+    <div className="col align-self-center">
+      <div className="image-card align-midle">
+        <Image
+          src={`https://picsum.photos/800/600?random=${index}`}
+          alt=""
+          rounded
+        />
+        <div className="context-menu-top">
+          <Button
+            variant="outline-light"
+            size="sm"
+            onClick={() => console.log("Context")}
+          >
+            <FiMoreVertical />
+          </Button>
+        </div>
+        <div className="context-menu-bottom">
+          <ButtonGroup>
+            <Button
+              variant="outline-light"
+              size="sm"
+              onClick={() => console.log("Edit")}
+            >
+              <AiOutlineEdit />
+            </Button>
+            <Button
+              variant="outline-light"
+              size="sm"
+              onClick={() => console.log("View")}
+            >
+              <AiOutlineEye />
+            </Button>
+            <Button
+              variant="outline-light"
+              size="sm"
+              onClick={() => console.log("Delete")}
+            >
+              <AiOutlineDelete />
+            </Button>
+          </ButtonGroup>
+        </div>
+      </div>
+    </div>
+  );
 }
