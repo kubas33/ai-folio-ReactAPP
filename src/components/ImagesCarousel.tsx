1d7c840a09e7309f1af2f430f2e35a5e5982ec9e
pow4h <44829224+kubas33@users.noreply.github.com>
2023-10-04 11:32:45 +0000

    Add carousel with images per category

---
+++ src/components/ImagesCarousel.tsx
@@ -0,0 +1,62 @@
+import Carousel from "react-multi-carousel";
+import ImageCard from "./ImageCard";
+
+const responsive = {
+  desktop: {
+    breakpoint: { max: 3000, min: 1024 },
+    items: 3,
+    partialVisibilityGutter: 60,
+  },
+  tablet: {
+    breakpoint: { max: 1024, min: 464 },
+    items: 2,
+    partialVisibilityGutter: 50,
+  },
+  mobile: {
+    breakpoint: { max: 464, min: 0 },
+    items: 1,
+    partialVisibilityGutter: 30,
+  },
+};
+
+export default function ImagesCarousel() {
+  return (
+    <div className="carousel-container">
+      <Carousel
+        additionalTransfrom={0}
+        arrows
+        autoPlaySpeed={3000}
+        centerMode={false}
+        className=""
+        containerClass="container"
+        dotListClass=""
+        draggable
+        focusOnSelect={false}
+        infinite
+        itemClass="p-1"
+        keyBoardControl
+        minimumTouchDrag={80}
+        partialVisible
+        pauseOnHover
+        renderArrowsWhenDisabled={false}
+        renderButtonGroupOutside={false}
+        renderDotsOutside={true}
+        responsive={responsive}
+        rewind={false}
+        rewindWithAnimation={false}
+        rtl={false}
+        shouldResetAutoplay
+        showDots={true}
+        sliderClass=""
+        slidesToSlide={1}
+        swipeable
+      >
+        {Array(8)
+          .fill()
+          .map((_, i) => (
+            <ImageCard key={i} />
+          ))}
+      </Carousel>
+    </div>
+  );
+}
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,60 +1,114 @@
-import { useEffect, useState } from "react";
-import ImageCard from '../components/ImageCard';
-import {Container, Row} from 'react-bootstrap';
+import { Fragment, useEffect, useState } from "react";
+import ImageCard from "../components/ImageCard";
+import { Container, Row } from "react-bootstrap";
+import ImagesCarousel from "../components/ImagesCarousel";
+import CategoryProps from "../interfaces/CategoryProps";
 
 interface LoadedImage {
-    fileName: string,
+  fileName: string;
+}
+
+async function getData(dataFile: string) {
+  try {
+    console.log("Fetching data");
+    const response = await fetch(`./src/data/${dataFile}.json`, {
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+    }); // Zakładając, że plik jest w tym samym katalogu co komponent
+    if (!response.ok) {
+      throw new Error("Błąd pobierania danych");
+    }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Błąd pobierania danych:", error);
+    throw error;
   }
+}
 
 export default function Home() {
+  const [images, setImages] = useState<LoadedImage[]>([]);
+  const [categories, setCategories] = useState<CategoryProps[]>([]);
 
-    const [images, setImages] = useState<LoadedImage[]>([]);
-
-    useEffect(() => {
-      setImages((prevState) => [...prevState, ...[
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const categoriesData = await getData("categories");
+        setCategories(categoriesData);
+        console.log({ categoriesData });
+      } catch (error) {
+        console.log({ error });
+      }
+    }
+    fetchData();
+    setImages((prevState) => [
+      ...prevState,
+      ...[
         {
-          fileName: '1.jpg'
+          fileName: "1.jpg",
         },
         {
-          fileName: '2.jpg'
+          fileName: "2.jpg",
         },
         {
-          fileName: '3.jpg'
+          fileName: "3.jpg",
         },
         {
-          fileName: '4.jpg'
+          fileName: "4.jpg",
         },
         {
-          fileName: '5.jpg'
+          fileName: "5.jpg",
         },
         {
-          fileName: '6.jpg'
+          fileName: "6.jpg",
         },
         {
-            fileName: '7.jpg'
-          },
-          {
-            fileName: '8.jpg'
-          },
-          {
-            fileName: '9.jpg'
-          },
-      ]])
-    }, [])
-
-    return(
-        <div className="container">
-            <div className="row">
-                <div className="col-md-12 text-center">
-                    <h1>AiFolio</h1>
-                </div>
-            </div>
-            <Container>
-                <div className="row row-cols-1 row-cols-sm-2 row-cols-lg-3">
-                    {images.map(image => <ImageCard fileName={image.fileName}/>)}
-                </div>
-
-            </Container>
+          fileName: "7.jpg",
+        },
+        {
+          fileName: "8.jpg",
+        },
+        {
+          fileName: "9.jpg",
+        },
+        {
+          fileName: "10.jpg",
+        },
+        {
+          fileName: "11.jpg",
+        },
+        {
+          fileName: "12.jpg",
+        },
+      ],
+    ]);
+  }, []);
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-md-12 text-center">
+          <h1>AiFolio</h1>
+        </div>
       </div>
-    )
+      <Container>
+        <div className="row row-cols-1 row-cols-sm-2 row-cols-lg-3">
+          {images.map((image) => (
+            <ImageCard fileName={image.fileName} key={image.fileName} />
+          ))}
+        </div>
+      </Container>
+      <hr />
+      <section className="categories-preview">
+        {categories.map((category) => (
+          <Fragment key={category.slug}>
+            <h3>{category.name}</h3>
+            <ImagesCarousel />
+            <hr />
+          </Fragment>
+        ))}
+      </section>
+    </div>
+  );
 }
