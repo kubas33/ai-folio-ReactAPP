f17f93a704e8a2ed4c76702ade0003a4c3168cf2
pow4h <44829224+kubas33@users.noreply.github.com>
2023-10-04 11:48:10 +0000

    Change navbar brand and add aria-role

--- src/components/MainNavbar.tsx
+++ src/components/MainNavbar.tsx
@@ -1,15 +1,15 @@
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
 
 export default function MainNavbar() {
-    return (
+  return (
     <Navbar expand="lg" className="bg-body-tertiary">
       <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Brand href="#home">AI-Folio</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
+        <Navbar.Collapse role={"navigation"} id="basic-navbar-nav">
           <Nav className="me-auto">
             <Nav.Link href="#home">Home</Nav.Link>
             <Nav.Link href="#link">Models</Nav.Link>
@@ -18,20 +18,16 @@ export default function MainNavbar() {
             <Nav.Link href="#link">Categories</Nav.Link>
           </Nav>
           <Nav>
-          <NavDropdown title="User" id="basic-nav-dropdown" align={'end'}>
+            <NavDropdown title="User" id="basic-nav-dropdown" align={"end"}>
               <NavDropdown.Item href="#action/3.1">Profile</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Settings
-              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">Settings</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.3">Smth</NavDropdown.Item>
               <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-              Logout
-              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.4">Logout</NavDropdown.Item>
             </NavDropdown>
           </Nav>
         </Navbar.Collapse>
       </Container>
     </Navbar>
-    );
+  );
 }
