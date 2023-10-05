e5721a1175626f28f783166d8bdd5b05b05d0af1
pow4h <44829224+kubas33@users.noreply.github.com>
2023-10-05 08:08:43 +0000

    Add background to main element

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import "react-multi-carousel/lib/styles.css";
 import MainNavbar from "./components/MainNavbar";
 import Home from "./pages/Home";
 import ImageDetail from "./pages/ImageDetail";
+import AddImageForm from "./components/AddImageForm";
 
 function App() {
   return (
@@ -12,9 +13,10 @@ function App() {
       <MainNavbar />
 
       <hr />
-      <main className="vh-100">
-        {/* <Home /> */}
-        <ImageDetail />
+      <main className="bg-body-tertiary">
+        {/* <Home /> 
+        <ImageDetail />*/}
+        <AddImageForm />
       </main>
     </>
   );
