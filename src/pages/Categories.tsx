59056aa56b1166dbe982400ffacb3734ba3a3811
kubas33 <Stasiak91@gmail.com>
2023-10-08 22:01:30 +0200

    Add array of categories

--- src/pages/Categories.tsx
+++ src/pages/Categories.tsx
@@ -1,7 +1,44 @@
+import { Box, Heading, Text } from "@chakra-ui/react";
+
+interface CategoriesProps {
+  imageUrl: string;
+  text: string;
+}
+
+let categories: string[] = [
+  'Animals',
+  'Beauty',
+  'Business',
+  'Cars and motorcycles',
+  'Celebrations and holidays',
+  'Cities',
+  'Education',
+  'Family',
+  'Fashion',
+  'Finance',
+  'Food and drink',
+  'Health and wellness',
+  'Holidays',
+  'Home',
+  'Industry',
+  'Music',
+  'Nature',
+  'People',
+  'Places',
+  'Science',
+  'Shopping',
+  'Sports and recreation',
+  'Technology',
+  'The arts',
+  'Transportation',
+  'Vintage'
+];
 
 
-export default function Categories() {
+export default function Categories({ imageUrl, text }: (CategoriesProps)) {
   return (
-    <div>Categories</div>
+    <>
+
+    </>
   )
 }
