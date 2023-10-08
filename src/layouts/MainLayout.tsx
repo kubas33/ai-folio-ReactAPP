5bc80bd25d487b329dd9e599eb6d873abd9efaa1
kubas33 <Stasiak91@gmail.com>
2023-10-08 16:32:00 +0200

    Change styling to Chakra

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,22 +1,25 @@
 
+import { Box, Center, useColorModeValue, Container } from '@chakra-ui/react';
 import MainFooter from '../components/MainFooter';
 import MainNavbar from '../components/MainNavbar';
 import { Outlet } from 'react-router-dom';
 
-export default function MainLayout(){
- 
-    return (
-        <>
-          <MainNavbar />
-    
-          <hr />
-          <main className="bg-body-tertiary">
-            {/* <Home /> */}
-            {/* <ImageDetail /> */}
-            {/* <AddImageForm /> */}
-            <Outlet/>
-          </main>
-            <MainFooter/>
-        </>
-      );
-  }
+export default function MainLayout() {
+
+  return (
+    <>
+      <MainNavbar />
+
+
+      <Container as='main' bg={useColorModeValue('gray.50', 'gray.900')}
+        color={useColorModeValue('gray.700', 'gray.200')}
+        maxW={'100%'}>
+        {/* <Home /> */}
+        {/* <ImageDetail /> */}
+        {/* <AddImageForm /> */}
+        <Outlet />
+      </Container>
+      <MainFooter />
+    </>
+  );
+}
