f5ea595c109d45897c4ec1306abb143f2985a064
pow4h <44829224+kubas33@users.noreply.github.com>
2023-10-09 11:59:50 +0000

    Improved styling

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,25 +1,23 @@
-
-import { Box, Center, useColorModeValue, Container } from '@chakra-ui/react';
-import MainFooter from '../components/MainFooter';
-import MainNavbar from '../components/MainNavbar';
-import { Outlet } from 'react-router-dom';
+import { Box, Center, useColorModeValue, Container } from "@chakra-ui/react";
+import MainFooter from "../components/MainFooter";
+import MainNavbar from "../components/MainNavbar";
+import { Outlet } from "react-router-dom";
 
 export default function MainLayout() {
-
   return (
-    <>
+    <Box
+      width={"100%"}
+      bg={useColorModeValue("gray.50", "gray.900")}
+      color={useColorModeValue("gray.700", "gray.200")}
+    >
       <MainNavbar />
-
-
-      <Container as='main' bg={useColorModeValue('gray.50', 'gray.900')}
-        color={useColorModeValue('gray.700', 'gray.200')}
-        maxW={'100%'}>
+      <Container as="main" maxW={"1440px"}>
         {/* <Home /> */}
         {/* <ImageDetail /> */}
         {/* <AddImageForm /> */}
         <Outlet />
       </Container>
       <MainFooter />
-    </>
+    </Box>
   );
 }
