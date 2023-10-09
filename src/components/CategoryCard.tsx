b6c2c67347eeb03b0d4c3a10c52798248a5dc663
pow4h <44829224+kubas33@users.noreply.github.com>
2023-10-09 11:58:19 +0000

    Added category browser

--- src/components/CategoryCard.tsx
+++ src/components/CategoryCard.tsx
@@ -1,34 +1,43 @@
-
-import { Box, Heading, Text } from '@chakra-ui/react';
+import { Box, Text } from "@chakra-ui/react";
+import { GlobalStyleProps } from "@chakra-ui/theme-tools";
 
 type Props = {
-    imageUrl: string;
-    text: string;
-}
+  imageUrl: string;
+  text: string;
+  imageUrl_2: string;
+};
 
-export default function CategoryCard({ imageUrl, text }: Props) {
-    return (
-        <>
-            <Heading>Browse categories</Heading>
-            <Box position="relative">
-                <Box
-                    backgroundImage={`url(${imageUrl})`}
-                    backgroundSize="cover"
-                    width="400px"
-                    aspectRatio="1"
-                >
-                    <Text
-                        position="absolute"
-                        top="50%"
-                        left="50%"
-                        transform="translate(-50%, -50%)"
-                        textAlign="center"
-                        color="white"
-                        fontSize="24px" // Dostosować wielkość tekstu według potrzeb
-                    >
-                        {text}
-                    </Text>
-                </Box>
-            </Box></>
-    )
+export default function CategoryCard({ imageUrl, imageUrl_2, text }: Props) {
+  const onHover: GlobalStyleProps = {
+    backgroundImage: `url(${imageUrl_2})`,
+    cursor: "pointer",
+    transition: "background-image 0.2s ease-in-out 0.1s",
+  };
+  return (
+    <>
+      <Box position="relative" width={"100%"} aspectRatio="1">
+        <Box
+          backgroundImage={`url(${imageUrl})`}
+          backgroundSize="cover"
+          width={"100%"}
+          aspectRatio="1"
+          filter="brightness(80%)"
+          borderRadius={"lg"}
+          _hover={onHover}
+        ></Box>
+        <Text
+          position="absolute"
+          top="50%"
+          left="50%"
+          transform="translate(-50%, -50%)"
+          textAlign="center"
+          color="white"
+          fontSize="24px" // Dostosować wielkość tekstu według potrzeb
+          fontWeight="600"
+        >
+          {text}
+        </Text>
+      </Box>
+    </>
+  );
 }
--- src/pages/Categories.tsx
+++ src/pages/Categories.tsx
@@ -1,44 +1,72 @@
-import { Box, Heading, Text } from "@chakra-ui/react";
+import {
+  Box,
+  Center,
+  Heading,
+  ListItem,
+  SimpleGrid,
+  UnorderedList,
+} from "@chakra-ui/react";
+import CategoryCard from "../components/CategoryCard";
 
 interface CategoriesProps {
   imageUrl: string;
+  imageUrl_2: string;
   text: string;
 }
 
-let categories: string[] = [
-  'Animals',
-  'Beauty',
-  'Business',
-  'Cars and motorcycles',
-  'Celebrations and holidays',
-  'Cities',
-  'Education',
-  'Family',
-  'Fashion',
-  'Finance',
-  'Food and drink',
-  'Health and wellness',
-  'Holidays',
-  'Home',
-  'Industry',
-  'Music',
-  'Nature',
-  'People',
-  'Places',
-  'Science',
-  'Shopping',
-  'Sports and recreation',
-  'Technology',
-  'The arts',
-  'Transportation',
-  'Vintage'
+const categories: string[] = [
+  "Animals",
+  "Beauty",
+  "Business",
+  "Cars and motorcycles",
+  "Celebrations and holidays",
+  "Cities",
+  "Education",
+  "Family",
+  "Fashion",
+  "Finance",
+  "Food and drink",
+  "Health and wellness",
+  "Holidays",
+  "Home",
+  "Industry",
+  "Music",
+  "Nature",
+  "People",
+  "Places",
+  "Science",
+  "Shopping",
+  "Sports and recreation",
+  "Technology",
+  "The arts",
+  "Transportation",
+  "Vintage",
 ];
 
-
-export default function Categories({ imageUrl, text }: (CategoriesProps)) {
+export default function Categories() {
   return (
-    <>
-
-    </>
-  )
+    <Center>
+      <Box width={"100%"} textAlign="center">
+        <Heading py={10}>Browse categories</Heading>
+        <UnorderedList listStyleType={"none"}>
+          <SimpleGrid columns={{ base: 1, md: 2, lg: 3 }} spacing={4}>
+            {categories.map((category) => {
+              const index: number = Math.floor(Math.random() * 100);
+              return (
+                <ListItem key={category} maxWidth={"400px"}>
+                  <CategoryCard
+                    text={category}
+                    imageUrl={`https://picsum.photos/600/600?random=${index}`}
+                    imageUrl_2={`https://picsum.photos/600/600?random=${
+                      index + 1
+                    }`}
+                  />
+                </ListItem>
+              );
+            })}
+          </SimpleGrid>
+        </UnorderedList>
+      </Box>
+    </Center>
+  );
 }
