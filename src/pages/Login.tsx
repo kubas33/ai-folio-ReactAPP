612eab18699b17a6fa80a93c932415fa85e02d31
pow4h <44829224+kubas33@users.noreply.github.com>
2023-10-10 11:58:34 +0000

    Update Login.tsx and Register.tsx

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,88 +1,127 @@
-import { Box, Button, Checkbox, Flex, FormControl, FormErrorMessage, FormLabel, Heading, Input, Stack, Text, useColorModeValue } from "@chakra-ui/react";
-import { useForm, SubmitHandler } from "react-hook-form"
+import {
+  Box,
+  Button,
+  Checkbox,
+  Flex,
+  FormControl,
+  FormErrorMessage,
+  FormLabel,
+  Heading,
+  Input,
+  Link,
+  Stack,
+  Text,
+  useColorModeValue,
+} from "@chakra-ui/react";
+import { useForm, SubmitHandler } from "react-hook-form";
 import { authorize } from "../store/authSlice";
 import { useDispatch } from "react-redux";
-
+import { useNavigate } from "react-router-dom";
+import { Link as ReactRouterLink } from "react-router-dom";
 
 type LoginForm = {
-    email: string,
-    password: string,
-}
+  email: string;
+  password: string;
+};
 
 export default function Login() {
-    const dispatch = useDispatch();
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<LoginForm>();
 
-    const {
-        register,
-        handleSubmit,
-        formState: { errors }
-    } = useForm<LoginForm>();
+  const onSubmit: SubmitHandler<LoginForm> = (data) => {
+    console.log(data);
+    dispatch(
+      authorize({
+        email: data.email,
+        name: "Kuba",
+        token: "1234351232",
+      }),
+    );
+    navigate("/home");
+  };
 
-    const onSubmit: SubmitHandler<LoginForm> = data => {
-        console.log(data);
-        dispatch(authorize({
-            email: data.email,
-            name: 'Kuba',
-            token: '1234351232'
-        }))
-    }
-
-    return (
-        <Flex
-            minH={'100vh'}
-            align={'center'}
-            justify={'center'}
-            bg={useColorModeValue('gray.50', 'gray.800')}
+  return (
+    <Flex
+      minH={"100vh"}
+      align={"center"}
+      justify={"center"}
+      bg={useColorModeValue("gray.50", "gray.800")}
+    >
+      <Stack spacing={8} mx={"auto"} maxW={"lg"} py={12} px={6}>
+        <Stack align={"center"}>
+          <Heading fontSize={"4xl"}>Sign in to your account</Heading>
+          <Text fontSize={"lg"} color={"gray.600"}>
+            to upload some cool{" "}
+            <Text as={"span"} color={"blue.400"}>
+              content
+            </Text>{" "}
+            ✌️
+          </Text>
+        </Stack>
+        <Box
+          rounded={"lg"}
+          bg={useColorModeValue("white", "gray.700")}
+          boxShadow={"lg"}
+          p={8}
         >
-            <Stack spacing={8} mx={'auto'} maxW={'lg'} py={12} px={6}>
-                <Stack align={'center'}>
-                    <Heading fontSize={'4xl'}>Sign in to your account</Heading>
-                    <Text fontSize={'lg'} color={'gray.600'}>
-                        to enjoy all of our cool <Text color={'blue.400'}>features</Text> ✌️
-                    </Text>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <Stack spacing={4}>
+              <FormControl id="email" isInvalid={errors.email}>
+                <FormLabel htmlFor="email">Email address</FormLabel>
+                <Input
+                  id="email"
+                  {...register("email", { required: "Email is required" })}
+                  type="email"
+                />
+                <FormErrorMessage>
+                  {errors.email && errors.email.message}
+                </FormErrorMessage>
+              </FormControl>
+              <FormControl id="password">
+                <FormLabel htmlFor="password">Password</FormLabel>
+                <Input
+                  id="password"
+                  {...register("password", {
+                    required: "Password is required",
+                  })}
+                  type="password"
+                />
+              </FormControl>
+              <Stack spacing={10}>
+                <Stack
+                  direction={{ base: "column", sm: "row" }}
+                  align={"start"}
+                  justify={"space-between"}
+                >
+                  <Checkbox>Remember me</Checkbox>
+                  <Text color={"blue.400"}>Forgot password?</Text>
                 </Stack>
-                <Box
-                    rounded={'lg'}
-                    bg={useColorModeValue('white', 'gray.700')}
-                    boxShadow={'lg'}
-                    p={8}>
-                    <form onSubmit={handleSubmit(onSubmit)}>
-                        <Stack spacing={4}>
-                            <FormControl id="email" isInvalid={errors.email}>
-                                <FormLabel htmlFor='email'>Email address</FormLabel>
-                                <Input id="email" {...register('email', { required: 'Email is required' })} type="email" />
-                                <FormErrorMessage>
-                                    {errors.email && errors.email.message}
-                                </FormErrorMessage>
-                            </FormControl>
-                            <FormControl id="password">
-                                <FormLabel htmlFor='password'>Password</FormLabel>
-                                <Input id="password" {...register('password', {
-                                    required: 'Password is required'
-                                })} type="password" />
-                            </FormControl>
-                            <Stack spacing={10}>
-                                <Stack
-                                    direction={{ base: 'column', sm: 'row' }}
-                                    align={'start'}
-                                    justify={'space-between'}>
-                                    <Checkbox>Remember me</Checkbox>
-                                    <Text color={'blue.400'}>Forgot password?</Text>
-                                </Stack>
-                                <Button
-                                    bg={'blue.400'}
-                                    color={'white'}
-                                    _hover={{
-                                        bg: 'blue.500',
-                                    }}
-                                    type="submit">
-                                    Sign in
-                                </Button>
-                            </Stack>
-                        </Stack>
-                    </form>
-                </Box>
+                <Button
+                  bg={"blue.400"}
+                  color={"white"}
+                  _hover={{
+                    bg: "blue.500",
+                  }}
+                  type="submit"
+                >
+                  Sign in
+                </Button>
+              </Stack>
             </Stack>
-        </Flex>
-    )
+          </form>
+          <Text align={"center"} pt={6}>
+            Not a member?{" "}
+            <Link color={"blue.400"} as={ReactRouterLink} to={"/register"}>
+              Sign up
+            </Link>
+          </Text>
+        </Box>
+      </Stack>
+    </Flex>
+  );
 }
--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,11 +1,112 @@
+import {
+  Flex,
+  Box,
+  FormControl,
+  FormLabel,
+  Input,
+  InputGroup,
+  HStack,
+  InputRightElement,
+  Stack,
+  Button,
+  Heading,
+  Text,
+  useColorModeValue,
+  Link,
+} from "@chakra-ui/react";
+import { useState } from "react";
+import { ViewIcon, ViewOffIcon } from "@chakra-ui/icons";
+import { Link as ReactRouterLink } from "react-router-dom";
 
+function Register() {
+  const [showPassword, setShowPassword] = useState(false);
 
-type Props = {}
+  return (
+    <Flex
+      minH={"100vh"}
+      align={"center"}
+      justify={"center"}
+      bg={useColorModeValue("gray.50", "gray.800")}
+    >
+      <Stack spacing={8} mx={"auto"} maxW={"lg"} py={12} px={6}>
+        <Stack align={"center"}>
+          <Heading fontSize={"4xl"} textAlign={"center"}>
+            Sign up
+          </Heading>
+          <Text fontSize={"lg"} color={"gray.600"}>
+            to upload some cool{" "}
+            <Text as={"span"} color={"blue.400"}>
+              content
+            </Text>{" "}
+            ✌️
+          </Text>
+        </Stack>
+        <Box
+          rounded={"lg"}
+          bg={useColorModeValue("white", "gray.700")}
+          boxShadow={"lg"}
+          p={8}
+        >
+          <Stack spacing={4}>
+            <HStack>
+              <Box>
+                <FormControl id="firstName" isRequired>
+                  <FormLabel>First Name</FormLabel>
+                  <Input type="text" />
+                </FormControl>
+              </Box>
+              <Box>
+                <FormControl id="lastName">
+                  <FormLabel>Last Name</FormLabel>
+                  <Input type="text" />
+                </FormControl>
+              </Box>
+            </HStack>
+            <FormControl id="email" isRequired>
+              <FormLabel>Email address</FormLabel>
+              <Input type="email" />
+            </FormControl>
+            <FormControl id="password" isRequired>
+              <FormLabel>Password</FormLabel>
+              <InputGroup>
+                <Input type={showPassword ? "text" : "password"} />
+                <InputRightElement h={"full"}>
+                  <Button
+                    variant={"ghost"}
+                    onClick={() =>
+                      setShowPassword((showPassword) => !showPassword)
+                    }
+                  >
+                    {showPassword ? <ViewIcon /> : <ViewOffIcon />}
+                  </Button>
+                </InputRightElement>
+              </InputGroup>
+            </FormControl>
+            <Stack spacing={10} pt={2}>
+              <Button
+                loadingText="Submitting"
+                size="lg"
+                bg={"blue.400"}
+                color={"white"}
+                _hover={{
+                  bg: "blue.500",
+                }}
+              >
+                Sign up
+              </Button>
+            </Stack>
 
-function Register({ }: Props) {
-    return (
-        <div>Register</div>
-    )
+            <Text align={"center"} pt={6}>
+              Already a user?{" "}
+              <Link color={"blue.400"} as={ReactRouterLink} to={"/login"}>
+                Login
+              </Link>
+            </Text>
+          </Stack>
+        </Box>
+      </Stack>
+    </Flex>
+  );
 }
 
-export default Register
+export default Register;
